ab534d41652b8c0bcb3a80497f5a43e9d3555952
Giovanni <arcangioligiovanni1@gmail.com>
2020-11-03 09:54:12 +0100

    Creazione del progetto.

---
+++ Convertitore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Convertitore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
